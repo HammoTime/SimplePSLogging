55a05839e98e1131a02759bebac2efaeea7ac669
HammoTime <code@hammo.me>
2016-04-11 04:06:31 +1000

    Added Get-Help notation and file header.

--- Logging-Library.psm1
+++ Logging-Library.psm1
@@ -1,5 +1,99 @@
-﻿Function Write-Message
+﻿<#
+    Version:        1.0
+    Author:         Adam Hammond
+    Creation Date:  09/04/2016
+    Last Change:    Updated file with help information.
+    Description:    This library contains a simple way to present useful and nice-looking
+                    information out to the user at the console. It writes out a MessageType
+                    and the current date out to the user, along with a message.
+                    
+                    If enabled, the output will also be written to a file.
+    Link:           https://github.com/HammoTime/SimplePSLogging
+    License:        The MIT License (MIT)
+#>
+
+Function Write-Message
 {
+    <#
+        .SYNOPSIS
+        
+        Writes a message to the screen, and a file (if enabled)
+        
+        .DESCRIPTION
+        
+        Similar to Write-Host except it automatically adds the date/time and what kind
+        of message is being written, and also writes the output to a file if that has
+        been enabled.
+        
+        .PARAMETER Message
+        
+        What you want to write out.
+        
+        .PARAMETER MessageType
+        
+        The type of message you're writing out.
+         - INFO = Informational.
+         - DEBG = Debugging.
+         - ERRR = Error.
+         - WARN = Warning.
+         
+        .PARAMETER ForegroundColor
+         
+        The color you would like the text to be, must be part of the predefined list.
+        * This has no effect on the file output.
+         
+        .PARAMETER BackgroundColor
+         
+        The color you would like the background of the console to be, must be part of
+        the predefined list. * This has no effect on the file output.
+         
+        .PARAMETER NoDate
+         
+        Do not include the Date in the output.
+         
+        .PARAMETER NoMessageType
+         
+        Do not include the MessageType in the output.
+         
+        .EXAMPLE
+         
+        Write-Message 'This is an informational message.'
+         
+        Output:
+        [2016-04-01 00:00:00] [INFO]: This is an informational message.
+         
+        .EXAMPLE
+         
+        Write-Message 'This is an error message.' ERRR
+         
+        Output:
+        [2016-04-01 00:00:00] [ERRR]: This is an error message.
+         
+        .EXAMPLE
+         
+        Write-Message 'This is a message with no date.' -NoDate
+         
+        Output:
+        [INFO]: This is a message with no date.
+         
+        .EXAMPLE
+         
+        Write-Message 'This is a message with no message type.' -NoMessageType
+         
+        Output:
+        [2016-04-01 00:00:00]: This is a message with no message type.
+         
+        .EXAMPLE
+         
+        Write-Message 'This is a message with no date or message type.' -NoDate -NoMessageType
+         
+        Output:
+        This is a message with no date or message type.
+         
+        .LINK
+         
+        https://github.com/HammoTime/SimplePSLogging/
+    #>
     param
     (
         [Parameter(Mandatory=$True)]
@@ -98,6 +192,23 @@
 
 Function Write-BlankLine
 {
+    <#
+        .SYNOPSIS
+        
+        Writes a blank line out.
+        
+        .DESCRIPTION
+        
+        Writes a blank line out to the screen, and to a file if it has been enabled.
+        
+        .PARAMETER IgnoreLogging
+        
+        Tells the script whether to explicitly exclude the blank line from file log output.
+         
+        .LINK
+         
+        https://github.com/HammoTime/SimplePSLogging/
+    #>
     param(
         [Switch]
         $IgnoreLogging # Added as people may not want to include blank lines in their files on some occassions.
@@ -112,6 +223,45 @@ Function Write-BlankLine
 
 Function Write-ScriptHeader
 {
+        <#
+        .SYNOPSIS
+        
+        Writes out a Microsoft-like header at the start of a script.
+        
+        .DESCRIPTION
+        
+        Writes out a Microsoft-like header at the start of a script.
+        
+        .PARAMETER ScriptName
+        
+        The name of the script you are running.
+        
+        .PARAMETER Version
+        
+        The current version of the script you are running.
+        
+        .PARAMETER CompanyName
+        
+        The company the script belongs to.
+        
+        .PARAMETER DontClearScreen
+        
+        A switch that tells the function whether it should run Clear-Host before
+        printing out the details to screen.
+         
+        .EXAMPLE
+         
+        Write-ScriptHeader 'Test Script' '98.1.3' 'Veridian Dynamics'
+         
+        Output:
+        Test Script [98.1.3]
+        (c) 2016 Veridian Dynamics. All rights reserved.
+        
+         
+        .LINK
+         
+        https://github.com/HammoTime/SimplePSLogging/
+    #>
     param
     (
         [Parameter(Mandatory=$True)]
@@ -138,6 +288,34 @@ Function Write-ScriptHeader
 
 Function Enable-LogWriting
 {
+        <#
+        .SYNOPSIS
+        
+        Enables logging to file for the SimplePSLogging library.
+        
+        .DESCRIPTION
+        
+        Sets a few global variables that are looked for by 'Write-Message'. Once
+        these are set, Write-Message will log every line out to the file given.
+        
+        .PARAMETER OutputLocation
+        
+        The file you want to write the output to.
+        * Folder must exist.
+        * File does not have to exist.
+         
+        .EXAMPLE
+         
+        Enable-LogWriting
+         
+        Output:
+        [2016-04-01 00:00:00] [INFO]: Logging is now enabled.
+        [2016-04-01 00:00:00] [INFO]: Log output will be available at 'C:\Temp\Log.txt'.
+         
+        .LINK
+         
+        https://github.com/HammoTime/SimplePSLogging/
+    #>
     param
     (
         [Parameter(Mandatory=$True)]
@@ -192,6 +370,27 @@ Function Enable-LogWriting
 
 Function Disable-LogWriting
 {
+        <#
+        .SYNOPSIS
+        
+        Disables logging to file for the SimplePSLogging library.
+        
+        .DESCRIPTION
+        
+        Unsets a few global variables that are looked for by 'Write-Message'. Once
+        these have been destroyed, Write-Message will no longer write output to file.
+         
+        .EXAMPLE
+         
+        Disable-LogWriting
+         
+        Output:
+        [2016-04-01 00:00:00] [INFO]: File logging now disabled on this system.
+         
+        .LINK
+         
+        https://github.com/HammoTime/SimplePSLogging/
+    #>
     $LoggingHasBeenDisabled = $False
     
     if($Global:FileLoggingEnabled)
